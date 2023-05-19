7bac587cdbe33f2a9aeaa53c91faa4b415a32d90
MaykerHM <hydeki8@gmail.com>
2023-05-19 18:23:16 -0300

    make all contact properties not null

--- src/main/java/com/elotech/people/domain/contact/Contact.java
+++ src/main/java/com/elotech/people/domain/contact/Contact.java
@@ -2,6 +2,7 @@ package com.elotech.people.domain.contact;
 
 import com.elotech.people.domain.person.Person;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
@@ -10,19 +11,23 @@ import java.util.UUID;
 public class Contact {
 
     @Id
+    @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @NotNull
     @Column(nullable = false, length = 50)
     private String name;
 
+    @NotNull
     @Column(nullable = false, length = 20)
     private String phoneNumber;
 
-
+    @NotNull
     @Column(nullable = false, length = 70)
     private String email;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "person_id")
     private Person person;
